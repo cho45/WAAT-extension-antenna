092866a3a89b6be8aafe63f1c758760740381024
cho45 <cho45@lowreal.net>
2014-02-15 16:41:02 +0900

    update

--- Gemfile
+++ Gemfile
@@ -4,3 +4,5 @@ source "https://rubygems.org"
 gem "json"
 gem "websocket-client-simple"
 
+gem "i2c-devices", :git => "git://github.com/cho45/ruby-i2c-devices.git", :branch => "master"
+
--- antenna.rb
+++ antenna.rb
@@ -7,43 +7,7 @@ Bundler.require
 require 'websocket-client-simple'
 require 'json'
 
-
-class I2CDevice
-	# ioctl command
-	# Ref. https://www.kernel.org/pub/linux/kernel/people/marcelo/linux-2.4/include/linux/i2c.h
-	I2C_RETRIES     = 0x0701
-	I2C_TIMEOUT     = 0x0702
-	I2C_SLAVE       = 0x0703
-	I2C_SLAVE_FORCE = 0x0706
-	I2C_TENBIT      = 0x0704
-	I2C_FUNCS       = 0x0705
-	I2C_RDWR        = 0x0707
-	I2C_SMBUS       = 0x0720
-	I2C_UDELAY      = 0x0705
-	I2C_MDELAY      = 0x0706
-
-	attr_accessor :address
-
-	def initialize(address)
-		@address = address
-	end
-
-	def i2cget(address, length=1)
-		i2c = File.open("/dev/i2c-1", "r+")
-		i2c.ioctl(I2C_SLAVE, @address)
-		i2c.write(address.chr)
-		ret = i2c.read(length)
-		i2c.close
-		ret
-	end
-
-	def i2cset(*data)
-		i2c = File.open("/dev/i2c-1", "r+")
-		i2c.ioctl(I2C_SLAVE, @address)
-		i2c.write(data.pack("C*"))
-		i2c.close
-	end
-end
+require "i2c"
 
 ws = WebSocket::Client::Simple.connect 'ws://localhost:51234'
 
